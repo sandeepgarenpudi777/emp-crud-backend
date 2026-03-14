9022aefd02eb5b4b6c36ae786bbe12564926f28a
sandeep <sandeepfullstackjava1@gmail.com>
2026-03-14 13:57:34 +0530

    change Emp.java and pom.xml

--- src/main/java/com/crud/entity/Emp.java
+++ src/main/java/com/crud/entity/Emp.java
@@ -5,10 +5,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
 @Table(name="empcrud")
-
+@Data
 public class Emp {
 	
 	@Id
